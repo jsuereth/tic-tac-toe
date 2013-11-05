886bb57b91c428a21f42cd6b23870c12ef97bb86
Maxime DANTEC <hello@warry.fr>
2013-11-05 18:22:02 +0100

    Merge

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,8 +2,12 @@ $(function() {
   var EMPTY_BOARD = [ [null, null, null],
                       [null, null, null],
                       [null, null, null]
-                     ];
-	
+                    ];
+
+  // Some names and adjectives for "random" nicknames
+  var animals = "Aardvark,Albatross,Alligator,Alpaca,Ant,Anteater,Antelope,Ape,Armadillo,Baboon,Badger,Barracuda,Bat,Bear,Beaver,Bee,Bison,Boar,Galago,Butterfly,Camel,Caribou,Cat,Caterpillar,Cattle,Chamois,Cheetah,Chicken,Chimpanzee,Chinchilla,Chough,Clam,Cobra,Cockroach,Cod,Cormorant,Coyote,Crab,Crocodile,Crow,Curlew,Deer,Dinosaur,Dog,Dolphin,Donkey,Dotterel,Dove,Dragonfly,Duck,Dugong,Dunlin,Eagle,Echidna,Eel,Elephant,Elk,Emu,Falcon,Ferret,Finch,Fish,Flamingo,Fly,Fox,Frog,Gaur,Gazelle,Gerbil,Giraffe,Gnat,Goat,Goose,Goldfish,Gorilla,Goshawk,Grasshopper,Grouse,Guanaco,Gull,Hamster,Hare,Hawk,Hedgehog,Heron,Herring,Hippopotamus,Hornet,Horse,Hummingbird,Hyena,Jackal,Jaguar,Jay,Jellyfish,Kangaroo,Koala,Kouprey,Kudu,Lapwing,Lark,Lemur,Leopard,Lion,Llama,Lobster,Locust,Loris,Louse,Lyrebird,Magpie,Mallard,Manatee,Marten,Meerkat,Mink,Monkey,Moose,Mouse,Mosquito,Mule,Narwhal,Newt,Nightingale,Octopus,Okapi,Opossum,Oryx,Ostrich,Otter,Owl,Ox,Oyster,Parrot,Partridge,Peafowl,Pelican,Penguin,Pheasant,Pig,Pigeon,Pony,Porcupine,Porpoise,Quail,Quelea,Rabbit,Raccoon,Rat,Raven,Reindeer,Rhinoceros,Ruff,Salamander,Salmon,Sandpiper,Sardine,Scorpion,Seahorse,Shark,Sheep,Shrew,Shrimp,Skunk,Snail,Snake,Spider,Squid,Squirrel,Starling,Stingray,Stinkbug,Stork,Swallow,Swan,Tapir,Tarsier,Termite,Tiger,Toad,Trout,Turtle,Vulture,Wallaby,Walrus,Wasp,Weasel,Whale,Wolf,Wolverine,Wombat,Woodcock,Woodpecker,Worm,Wren,Yak,Zebra".split(",");
+  var adjectives = "Amazing,Angry,Black,Cunning,Dark,Dangerous,Fantastic,Flash,Flying,Great,Green,Grey,Magic,Nasty,Rapid,Reactive,Red,Silly,Speedy,Stealthy,Striking,Stunning,Swift,The,White,Wicked,Wild".split(",");
+
   // Helper ot handle binding to a socket and dealing with messages.
   var connection = (function(){
     var socket = new WebSocket(jsRoutes.controllers.SocketController.connectToGameServer().webSocketURL());
@@ -14,7 +18,7 @@ $(function() {
       alive(true);
       console.log("SOCKET OPEN: ", e);
       for(var i = 0; i < bufferedMessages.length; ++i) {
-    	  sendMessage(bufferedMessages[i]);
+        sendMessage(bufferedMessages[i]);
       }
       bufferedMessages = [];
     };
@@ -23,7 +27,7 @@ $(function() {
       alive(false);
       console.log("SOCKET ERROR: ", e);
     };
-    
+
     function sendMessage(jsonOrString) {
       var msg = typeof jsonOrString == 'string' ? jsonOrString :  JSON.stringify(jsonOrString);
       if(!alive()) {
@@ -35,7 +39,7 @@ $(function() {
 
     function setHandler(handler) {
        var realListener = function(msg) {
-    	  // TODO - Handle failrues
+        // TODO - Handle failrues
           var obj = JSON.parse(msg.data);
           console.debug('SOCKET Received: ', obj);
           handler(obj)
@@ -48,67 +52,58 @@ $(function() {
       setHandler: setHandler
     }
   })();
-  
+
   // A custom knockout binding so that we can directly take the board
   // status updates and use them.
   ko.bindingHandlers.board = {
     init: function(element, valueAccessor, allBindings) {
-    	// The accessor gives us a function that returns the value.
-        var rawBoard = valueAccessor();
-        var board = ko.unwrap(rawBoard);
         // Here we grab a click handler
+        // We could use a direct reference to self.move as well
+        // But since we are in a component let's avoid side effects
         var clickHandler = allBindings().boardClick || function() {};
-        
+
         // Now we create our DOM elements....
-        var buf = [];
         // We don't actually need to write out the text values here, because
         // Update will be called immediately after init.  So we just
         // layout the structure and click listeners, nothing more.
         for(var row = 0; row < 3; ++row) {
-        	for(var col = 0; col < 3; ++ col) {
-        		var value = board[row][col]
-        		buf.push('<div class="box" data-row="', row, '"')
-        		buf.push(' data-col="', col, '"></div>')
-        	}
+          for(var col = 0; col < 3; ++ col) {
+            $('<div class="box"/>').click(function(){
+              clickHandler(row, col);
+            }).appendTo(element);
+          }
         }
-        var html = buf.join('')
-        element.innerHTML = html;
-        // Register our listener.
-        var clicker = function(e) {
-        	var el = $(this);
-        	var row = Number(el.attr('data-row'));
-        	var col = Number(el.attr('data-col'));
-        	clickHandler(row, col);
-        }
-        $(element).children('div').each(function(idx, el) {
-        	$(el).click(clicker);
-        });
     },
     update: function(element, valueAccessor) {
         var rawBoard = valueAccessor();
         var board = ko.unwrap(rawBoard);
         // Update values in the board.
         $(element).children('div').each(function(idx, el) {
-        	var e = $(el);
-        	var row = e.attr('data-row');
-        	var col = e.attr('data-col');
-        	var value = board[row][col];
-        	e.text(value ? value : '');
+          var e = $(el);
+          var row = Math.floor(idx/3);
+          var col = idx%3;
+          var value = board[row][col];
+          if (!!value){
+            e.text(value);
+            // Unbind click, not mandatory but avoid useless calls
+            e.off("click");
+          }
         });
-        // TODO - See if we need to update the click handler..
     }
   };
-  
+
 
 
   // The board class.
   var Board = function() {
     var self = this;
+    var now = new Date(); // For "random" nickname
+    var nickname = adjectives[now%adjectives.length]+" "+animals[now%animals.length];
 
     // ------ Direct State ------------------------------------------
     // Here are the observables that contain the raw state of the UI.
     // --------------------------------------------------------------
-    self.user = ko.observable(''); // The name we use when playing others.
+    self.user = ko.observable(nickname); // The name we use when playing others.
     self.id = ko.observable();  // The current game id.
     self.board = ko.observable(EMPTY_BOARD);  // The state of the board.
     self.player = ko.observable();  // Our move type (x or o)
@@ -116,24 +111,26 @@ $(function() {
     self.xPlayer = ko.observable(); // The name of the x player
     self.oPlayer = ko.observable(); //  The name of the o player
     self.loggedIn = ko.observable(false); // Whether we logged in with our name or not.
-    
-    
-    
+    self.turn = ko.observable(false); // Who's turn (true = me)
+
     // ------ Derived State --------------------------------------
     // Here are the *computed* observables we use to drive the UI.
     // -----------------------------------------------------------
-    
+    self.appPage = ko.computed(function(){
+      return !self.loggedIn()?"home":"game";
+    });
+
     // Are we involved in a game?
     self.hasActiveGame = ko.computed(function(){
-    	switch(self.player()) {
-    	case 'x':
-    	case 'o':
-    		return true;
+      switch(self.player()) {
+      case 'x':
+      case 'o':
+        return true;
         default:
-        	return false;
-    	}
+          return false;
+      }
     });
-    
+
     // Are we still looking for someone to play?
     self.isSearchingOpponent = ko.computed(function(){
         return self.player() == 'x' && !self.oPlayer();
@@ -141,51 +138,50 @@ $(function() {
 
     // What text should we use for player X?
     self.xPlayerText = ko.computed(function() {
-    	if(self.player() == 'x') {
-    		return '<You>';
-    	}
-    	return self.xPlayer();
+      if(self.player() == 'x') {
+        return '<You>';
+      }
+      return self.xPlayer();
     });
 
     // What text should we use for player Y?
     self.oPlayerText = ko.computed(function() {
-    	if(self.player() == 'o') {
-    		return '<You>';
-    	}
-    	var tmp = self.oPlayer();
-    	return tmp ? tmp : 'Finding Opponent...';
+      if(self.player() == 'o') {
+        return '<You>';
+      }
+      var tmp = self.oPlayer();
+      return tmp ? tmp : 'Finding Opponent...';
     });
-    
+
     // Is the active game finished?
     self.isDone = ko.computed(function() {
-    	switch(self.winner()) {
-    	case 'tie':
-    	case 'x':
-    	case 'y':
-    		return true;
+      switch(self.winner()) {
+      case 'tie':
+      case 'x':
+      case 'y':
+        return true;
         default:
-        	return false;
-    	}
+          return false;
+      }
     });
-    
+
     // Did we tie?
     self.isTie = ko.computed(function() {
-    	return self.winner() == 'tie';
+      return self.isDone() && self.winner() == 'tie';
     });
     // Did we win?
     self.isWin = ko.computed(function() {
-    	return self.winner() == self.player();
+      return self.isDone() &&self.winner() == self.player();
     });
     // Did we lose?
     self.isLose = ko.computed(function() {
-    	return self.isDone() && !(self.isTie() || self.isWin());
+      return self.isDone() && !(self.isTie() || self.isWin());
     })
-    
+
     // ------ Responses -------------------------------
     // How we respond to messages from the game server.
     // ------------------------------------------------
 
-    
     // This will update the state of the game from a game state update message.
     function handleBoardState(msg) {
       // TODO - Check to see if msg.game = self.id()
@@ -194,31 +190,31 @@ $(function() {
       self.oPlayer(msg.o)
       self.winner(msg.winner)
     }
-    
+
     function handleJoinGame(obj) {
       self.player(obj.player);
       self.id(obj.game);
     }
-    
- // Here we register our handler for messages
+
+    // Here we register our handler for messages
     connection.setHandler(function(obj) {
         if(obj.response) {
           switch(obj.response) {
             case 'JoinGame':
-          	  handleJoinGame(obj);
-          	break;
+              handleJoinGame(obj);
+            break;
             case 'BoardState':
               handleBoardState(obj);
-          	break;
+            break;
           }
         }
       });
-    
+
     // ------ Actions -----------------------
-    // Methods that manipulate our state.  
+    // Methods that manipulate our state.
     // Can be from the server, or via the UI.
     // --------------------------------------
-    
+
     // UI Action - try to move at the given row + column
     self.move = function(row, col) {
       console.debug('Moving to (', row, ',', col, ')')
@@ -230,25 +226,23 @@ $(function() {
         connection.send({ request: 'BoardStateRequest', game: self.id() });
       }
     }
-    
+
     // UI Action - Try to find a new game.
     self.getNextGame = function() {
-    	// RESET our state to 'joining'
+      // RESET our state to 'joining'
         self.loggedIn(true);
         self.player(null);
         connection.send({
-      	  request: 'FindGameForPlayer',
-      	  player: self.user()
+          request: 'FindGameForPlayer',
+          player: self.user()
         });
     }
-    
-    
 
   }
-  
+
   $(function() {
-	  var board = new Board();
-	  window.board = board;
-	  ko.applyBindings(board); 
+    var board = new Board();
+    window.board = board;
+    ko.applyBindings(board);
   });
 });
